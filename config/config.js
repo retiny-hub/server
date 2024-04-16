e32bd7187e05cd970f69dd5335abffc1af269648
retiny-hub <166991905+retiny-hub@users.noreply.github.com>
2024-04-16 07:50:31 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,1 +1,18 @@
-
+module.exports = {
+    USER_SECRET:process.env.USER_SECRET,
+    APP_SECRET:process.env.APP_SECRET,
+    ACOOUNT_ACTIVATION_SECRET:process.env.ACOOUNT_ACTIVATION_SECRET,
+    PASSWORD_RESET_SECRET:process.env.PASSWORD_RESET_SECRET,
+    adminEmails:process.env.adminEmails,
+    mailPass:process.env.pass,
+    mailEmail:process.env.email,
+    mailPort:process.env.port,
+    mailHost:process.env.host,
+    MAILGUN_API_KEY:process.env.MAILGUN_API_KEY,
+    MAILGUN_API_SECRET:process.env.MAILGUN_API_SECRET,
+    DOMAINS:(process.env.NODE_ENV==='production'?[process.env.CLIENT_PROD_URL,process.env.API_PROD_URI]:
+    [process.env.CLIENT_DEV_URL,process.env.API_DEV_URI]),
+    CLIENT_URL:process.env.NODE_ENV==='production'?process.env.CLIENT_PROD_URL:process.env.CLIENT_DEV_URL,
+    API_URL:process.env.NODE_ENV==='production'?process.env.API_PROD_URI:process.env.API_DEV_URI,
+    DB:process.env.NODE_ENV==='production'?process.env.remoteDB:process.env.localDB
+}
